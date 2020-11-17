4f422eef2b18323e151dcdd13537bb11f04607f9
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-11-17 10:52:19 +0100

    Add files via upload

---
+++ beakovicPaolo4/beakovicPaolo4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaolo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijenu bez PDV-a : ");
+            double bezPDV = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Unesite cijenu sa PDV-om : ");
+            double saPDV = Convert.ToDouble(Console.ReadLine());
+
+            double PDV = ((saPDV / bezPDV) - 1) * 100;
+
+            Console.WriteLine("PDV izađe : " + PDV + "%");
+            Console.ReadKey();
+
+        }
+    }
+}
